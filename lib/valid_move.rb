1dab483456694bfcad4ca37c27818b55f2302ae9
Sean Xiong <seanxionghe@gmail.com>
2020-04-15 01:03:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,23 @@
 # code your #valid_move? method here
-def valid_move? (board, index)
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
- def position_taken? (board, index)
+# code a method to check if the next move is on the board at all
+# code the logic reationship that both (position is not taken) & (next move is on board) are true
+def valid_move? (board, index)
+  def position_taken? (board, index)
     if board[index] == "" || board[index] == " " ||board[index] == nil
       return false
     else board[index] == "O" || board[index] == "X"
       return true
     end
   end
-# code a method to check if the next move is on the board at all
+
  def on_board? (index)
    if index.between?(0,8) == true
      return true
    else
      return false
-# code the logic reationship that both (position is not taken) & (next move is on board) are true
+   end
+
  if (position_taken?(index) == false && on_board?(index) == true)
    return true
  else
